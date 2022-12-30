bf87158b8647e08e5d67e48620ffb86aab90f698
hung-yee,lin <43246276+celim1962@users.noreply.github.com>
2022-12-30 14:28:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,186 @@ const getAllFile = async () => {
     return result
 }
 
-app.get('/', (req, res) => res.send(`<h1>123</h1>`))
+app.get('/', (req, res) => res.send(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google" content="notranslate">
+    <title>會計搜尋</title>
+    <style>
+        * {
+            margin: 0;
+            box-sizing: border-box;
+            user-select: none;
+        }
+
+        .Display {
+            background-color: gray;
+
+            width: 100vw;
+            height: 30vh;
+
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+
+        .Display>div {
+            width: 80%;
+            height: 50%;
+
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+
+        .btnSelect {
+            background-color: rgb(185, 185, 185);
+            border: none;
+            width: 40%;
+            height: 50%;
+            cursor: pointer;
+        }
+
+        .btnSelect:hover {
+            background-color: rgb(220, 220, 220);
+            width: 41%;
+            height: 51%;
+        }
+
+        .btnSelect:active {
+            width: 40%;
+            height: 50%;
+        }
+
+        option {
+            font-size: 20px;
+            cursor: pointer;
+        }
+
+        select {
+            background-color: rgb(216, 212, 212);
+            width: 80%;
+            height: 50%;
+            cursor: pointer;
+        }
+
+        .result {
+            background-color: rgb(156, 156, 156);
+            height: 70vh;
+            overflow: auto;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <section class="Display">
+        <div>
+            <label>選擇類型</label>
+            <select id="getTypes">
+                <option value="收入">收入</option>
+                <option value="支出">支出</option>
+            </select>
+        </div>
+        <div>
+            <label>選擇科目</label>
+            <select name="details" id="details"></select>
+        </div>
+        <div>
+            <input type="button" class="btnSelect" value="新增項目">
+        </div>
+    </section>
+
+    <section class="result">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">類型</th>
+                    <th scope="col">科目</th>
+                    <th scope="col">摘要</th>
+                    <th scope="col">金額</th>
+                </tr>
+            </thead>
+            <tbody id="content">
+
+            </tbody>
+        </table>
+    </section>
+
+    <script>
+
+        let getTypes = document.getElementById('getTypes')
+        let details = document.getElementById('details')
+        let content = document.getElementById('content')
+
+        let allIncomeDetails = ['個人奉獻-婦女團契', '個人奉獻-讚美操', '聚會奉獻', '利息收入']
+        let allExpenseDetails = ['事工活動費', '公禱金', '負擔金', '慶慰費', '講師費', '對內奉獻-轉讚美操']
+
+        let createOption = (allItems) => {
+            details.innerHTML = ''
+
+            allItems.map(item => {
+                let temp = document.createElement('option')
+                temp.value = item
+                temp.innerText = item
+                details.appendChild(temp)
+            })
+
+            details.value = ''
+        }
+
+        let search = async (condition) => {
+            let res = await fetch(`https://celim1962api.onrender.com/getAllFile`)
+            console.log(res)
+            let allResult = await res.json()
+            return allResult
+        }
+
+        // Default value of incomeDetails
+        createOption(allIncomeDetails)
+
+
+        getTypes.addEventListener('change', () => {
+            let allDetails = getTypes[0].selected === true ? allIncomeDetails : allExpenseDetails;
+            createOption(allDetails)
+        })
+
+        details.addEventListener('change', async () => {
+            let result = await search(details.value)
+            let count = 1
+            content.innerHTML = ''
+
+            result.map(data => {
+                content.innerHTML += `<tr>
+                    <th scope="row">${count}</th>
+                    <td>${data[0]}</td>
+                    <td>${data[1]}</td>
+                    <td>${data[2]}</td>
+                    <td>${data[3]}</td>
+                </tr>`;
+                count++
+            })
+
+            if (result.length === 0) {
+                content.innerHTML = `<tr>
+                    <th scope="row">N/A</th>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                </tr>`;
+            }
+        })
+
+    </script>
+</body>
+
+</html>`))
 
 app.get('/test',(req,res)=>{
   return res.json('123test')
